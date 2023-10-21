f8a7ac73e6475cd084c00a36e3eafbb18fd57747
sabrallah <sabrallah1991@gmail.com>
2023-10-21 16:36:59 -0700

    task7

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
-from flask import Flask, render_template
-from models import *
+"""web app  must listening on 0.0.0.0"""
+from flask import Flask
+from flask import render_template
 from models import storage
+from models.state import State
+from models.city import City
+
+
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
+    """Cities by States"""
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
